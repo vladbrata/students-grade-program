b6475dbf8dcc21e58d3e762c7b1969259de57228
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-05-07 18:30:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+string filePath = @"D:\Coding\CSharpProjects\school-grade-database\students.txt";
+
+StreamReader? sr = new StreamReader(filePath);
+var liceu1 = new Clasa("9D");
+Student? student = null;
+while (sr.Peek() != -1) {
+    string?[] data = sr.ReadLine().Split(", ");
+    string? studentName = data[0];
+    student = new Student(studentName, liceu1);
+    liceu1.StudentList.Add(student);
+    for (int i = 1; i < data.Length; i++) {
+        student.GradeList.Add(new Grade(data[i]));
+    }
+}
+sr.Close();
+for (int i = 0; i < liceu1.StudentList.Count; i++) {
+    liceu1.StudentList[i].PrintInfo();
+    Console.WriteLine();
+    Console.WriteLine();
+}
